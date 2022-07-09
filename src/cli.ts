bf48ae1e62afc450c4480dc76980d6fcc20d2c18
cccxiv <108616801+cccxiv@users.noreply.github.com>
2022-07-09 13:01:42 +0700

    chore: re-format code using prettier

--- src/cli.ts
+++ src/cli.ts
@@ -2,7 +2,4 @@ import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
 import start from "./cli/start";
 
-yargs(hideBin(process.argv))
-  .command(start)
-  .help()
-  .argv
+yargs(hideBin(process.argv)).command(start).help().argv;
--- src/cli/start.ts
+++ src/cli/start.ts
@@ -1,29 +1,29 @@
-import { CommandModule } from "yargs"
-import Fastify from 'fastify'
+import { CommandModule } from "yargs";
+import Fastify from "fastify";
 
-const command = 'start'
+const command = "start";
 
-const describe = 'Start Olah server'
+const describe = "Start Olah server";
 
 const handler = async function () {
   const fastify = Fastify({
-    logger: true
-  })
+    logger: true,
+  });
 
-  fastify.get('/', function (request, reply) {
-    reply.send({ hello: 'world' })
-  })
+  fastify.get("/", function (request, reply) {
+    reply.send({ hello: "world" });
+  });
 
   fastify.listen({ port: 3000 }, function (err, address) {
     if (err) {
-      fastify.log.error(err)
-      process.exit(1)
+      fastify.log.error(err);
+      process.exit(1);
     }
-  })
-}
+  });
+};
 
 export default {
   command,
   describe,
-  handler
-} as CommandModule
+  handler,
+} as CommandModule;
